c2d4cbdf2303a20f1bd8ac9389e42427526aee66
Aung Ko Min <aung_ko_min@transitlink.com.sg>
2022-09-05 00:13:23 +0800

    Initial Commit

---
+++ SelectableWorldMap/SelectableWorldMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelectableWorldMapApp.swift
+//  SelectableWorldMap
+//
+//  Created by Aung Ko Min on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SelectableWorldMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
